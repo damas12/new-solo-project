41cf43275ae9d161f07995c8c0876755269a6d68
Damian Fusiek <damas13251@gmail.com>
2021-04-09 19:13:43 +0200

    Links container done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,37 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars	
+
+function toggleMenu(visible) {
+  document.querySelector('.panel').classList.toggle('show', visible);
+}
+document.querySelector('.hamburger').addEventListener('click', function (e) {
+  e.preventDefault();
+  toggleMenu();
+});
+
+/* function closeModal() {
+  document.getElementById('overlay').classList.remove('show');
+}
+document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+document.querySelector('#overlay').addEventListener('click', function (e) {
+  if (e.target === this) {
+    closeModal();
+  }
+});
+document.addEventListener('keyup', function (e) {
+  if (e.keyCode === 27) {
+    closeModal();
+  }
+});
+
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function (modal) {
+    modal.classList.remove('show');
+  });
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
+} */
